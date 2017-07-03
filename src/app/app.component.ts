5cdaacc776f0cd398d0a6611a338dac9a7fc109e
JorgeEnrique21 <jorgenrique_azevedo@yahoo.com.br>
2017-07-03 10:36:12 -0300

    Até a parte de estilizar do passo 3, Hail Minora

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,33 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-
-template: `<h1>{{title}}</h1>
-  <h2>{{hero.name}} details!</h2>
-  <div><label>id: </label>{{hero.id}}</div>
-  <div><label>name: </label>{{hero.name}}</div>`,
-  styleUrls: ['./app.component.css']
-})
 export class Hero {
   id: number;
   name: string;
 }
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Fabiano' },
+  { id: 12, name: 'Xandampião' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
 export class AppComponent {
   title = 'Tour of Heroes';
-  hero: Hero = {
-   id: 1,
-   name: 'Windstorm'
+  selectedHero : Hero = undefined;
+  heroes = HEROES;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   };
 }
